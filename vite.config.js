297f74b24fa58b9c0488cc9610d795c41cf0774b
Aaron Chen <aaronchen@Aarons-MacBook-Air.local>
2026-02-12 19:56:52 -0500

    change base setting to github repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set to '/repo-name/' if deploying to https://<USERNAME>.github.io/<REPO>/
+  base: '/personal_website/', // Set to '/repo-name/' if deploying to https://<USERNAME>.github.io/<REPO>/
 })
