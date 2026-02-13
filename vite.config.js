47a1a3f532efef9369051595fd516ef9a9b13cc7
Aaron Chen <aaronchen@Aarons-MacBook-Air.local>
2026-02-12 20:41:58 -0500

    Update base path for user site

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal_website/', // Set to '/repo-name/' if deploying to https://<USERNAME>.github.io/<REPO>/
+  base: '/', // Set to '/repo-name/' if deploying to https://<USERNAME>.github.io/<REPO>/
 })
